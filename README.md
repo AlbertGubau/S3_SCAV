2f85e2ccfbfd87c0a715bdd123ab765909a9c4be
AlbertGubau <albert.gubau01@estudiant.upf.edu>
2022-11-15 10:11:59 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# S3_SCAV
-SCAV Seminar 3 Repository
+# S2_SCAV
+This is a GitHub repository that has been created to upload the results of SCAV S3.
+
+## Task 1 (Create the videos encoded with VP8, Vp9, h265 and AV1 codecs)
+This task has no script associated but we can see the results in the files (we use 480p BBB video of 20 seconds):
+
+· output_480_VP8.webm
+· output_480_VP9.webm
+· output_480_h265.mp4
+· output_480_av1.mkv
+
+## Task 2 (ex1.py)
+This script is able to create a video with the 4 encoded videos in mosaic form.
+
+## Task 3 (ex2.py)
+This script is able to create an interactive Tkinter interface in which users can convert their videos to the desired codecs (VP8, VP9, h265 and AV1).
+
+
---
+++ ex1.py
@@ -0,0 +1,30 @@
+# This script is able to generate a mosaic video of the 4 different video codecs (VP8, VP9, h265 and AV1)
+import os
+
+
+def mosaic_video(inputfile1, inputfile2, inputfile3, inputfile4):
+
+    command_line = 'ffmpeg ' \
+                   '-i '+str(inputfile1)+' -i '+str(inputfile2)+' -i '+str(inputfile3)+' -i '+str(inputfile4)+' ' \
+                   '-filter_complex "' \
+                   'nullsrc=size=1280x720 [base]; ' \
+                        '[0:v] setpts=PTS-STARTPTS, scale=640x360 [upperleft];' \
+                        '[1:v] setpts=PTS-STARTPTS, scale=640x360 [upperright]; ' \
+                        '[2:v] setpts=PTS-STARTPTS, scale=640x360 [lowerleft]; ' \
+                        '[3:v] setpts=PTS-STARTPTS, scale=640x360 [lowerright]; ' \
+                        '[base][upperleft] overlay=shortest=1 [tmp1]; ' \
+                        '[tmp1][upperright] overlay=shortest=1:x=640 [tmp2]; ' \
+                        '[tmp2][lowerleft] overlay=shortest=1:y=360 [tmp3]; ' \
+                        '[tmp3][lowerright] overlay=shortest=1:x=640:y=360' \
+                   '" ' \
+                   '-c:v libx264 output_4_videos.mkv'
+
+    os.system(command_line)
+
+
+input_name1 = "output_480_VP8.webm"
+input_name2 = "output_480_VP9.webm"
+input_name3 = "output_480_h265.mp4"
+input_name4 = "output_480_av1.mkv"
+
+mosaic_video(input_name1, input_name2, input_name3, input_name4)
